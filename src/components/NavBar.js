ecf4bb9967dfbbd0b85d05550fcfdb04091764e6
Drona Akshay Kumar Devarasetty <dronaakshaykumar@gmail.com>
2021-11-29 00:56:51 -0600

    displayed current weather information

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,58 @@
-import { Form , FormControl,  Button } from "react-bootstrap";
+import { Form , Button } from "react-bootstrap";
+import { useState } from "react";
 import '../styles/NavBar.css';
 
-const NavBar = () => {
+export default ({callBack}) => {
+
+    const [currentCity, setCurrentCity] = useState("");
+    const [placeholder, setPlaceholder] = useState("Enter the location");
+    const [labelState, setLabelState] = useState("SearchLabel");
+    
+    const handleInputChange = (oEvent) => {
+        if (labelState === "searchLabel searchWarning") {
+            setLabelState("searchLabel");
+            setPlaceholder("Enter the location");
+        } else {
+            setCurrentCity(oEvent.target.value);
+        }
+    }
+
+    const handleInputClick = ()=>{
+        if (labelState === "searchLabel searchWarning") {
+            setLabelState("searchLabel");
+            setPlaceholder("Enter the location");
+        }
+    }
+
+    const handleButtonClick = ()=>{
+        if (currentCity.trim(" ") === "") {
+            setLabelState("searchLabel searchWarning");
+            setPlaceholder("");
+        } else {
+            callBack(currentCity);
+            setLabelState("searchLabel");
+            setPlaceholder("Enter the location");
+        }
+    }
+
+    const handleKeyPress = (oEve)=>{
+        if (oEve.key === "Enter") handleButtonClick();
+        else handleInputClick();
+    }
+
     return (
         <nav className="navbar navbar-light bg-light justify-content-between">
             <h1 className="navbar-brand titleStyle px-3">Weather Info</h1>
             <Form className="d-flex">
-                <FormControl
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
+                <input
+                    value={currentCity}
+                    onChange={handleInputChange}
+                    onClick={handleInputClick}
+                    onKeyPress={handleKeyPress}
+                    className="SearchInput me-3"
+                    placeholder={placeholder}
                 />
-                <Button className="me-3" variant="secondary">Search</Button>
+                <Button className="me-3" onClick={handleButtonClick} variant="secondary">Search</Button>
             </Form>
         </nav>
     )
@@ -20,4 +60,3 @@ const NavBar = () => {
 }
 
 
-export default NavBar;
