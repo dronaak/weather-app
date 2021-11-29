f37c2e6e8cfecc852242323fe4e6342740ea83a4
dronaak <dronaakshaykumar@gmail.com>
2021-11-29 01:01:48 -0600

    Revert "Feature/forecast card"

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import './App.css';
-import NavBar from './components/NavBar';
-import ForecastCard from './components/ForecastCard';
-import React, { useState } from "react";
 
-const App = () => {
-  const [city, setCity] = useState("");
-
-  const getLocation = (target) => {
-    setCity(target);
-  }
-
+function App() {
   return (
     <div className="App">
-      <NavBar callBack={getLocation}/>
-      <ForecastCard location={city}/>
+      <h1>Hello World</h1>
     </div>
   );
 }
 
 export default App;
-
